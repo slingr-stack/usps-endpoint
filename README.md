6f1caaa0040f57d8be535c837ec996cfddb4d3b8
Hugo Pacini <hugodpn@gmail.com>
2019-07-04 15:40:40 -0300

    #9679 Create basic structure for usps endpoint.

---
+++ README.md
@@ -0,0 +1,17 @@
+# USPS endpoint
+
+## Overview
+
+## About SLINGR
+
+SLINGR is a low-code rapid application development platform that accelerates development, with robust architecture for integrations and executing custom workflows and automation.
+
+[More info about SLINGR](https://slingr.io)
+
+## License
+
+This endpoint is licensed under the Apache License 2.0. See the `LICENSE` file for more details.
+
+
+
+
---
+++ src/main/java/io/slingr/endpoints/usps/USPS.java
@@ -0,0 +1,23 @@
+package io.slingr.endpoints.usps;
+
+import io.slingr.endpoints.Endpoint;
+import io.slingr.endpoints.framework.annotations.SlingrEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * <p>
+ * <p>Created by hpacini on 03/07/19.
+ */
+@SlingrEndpoint(name = "usps", functionPrefix = "_")
+public class USPS extends Endpoint {
+
+    private Logger logger = LoggerFactory.getLogger(USPS.class);
+
+
+    public void endpointStarted() {
+    }
+
+
+}
