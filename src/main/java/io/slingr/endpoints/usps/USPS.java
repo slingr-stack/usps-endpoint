1da94a467e02450a881808d947b845d16d38ad2c
Hugo Pacini <hugodpn@gmail.com>
2019-07-05 17:16:59 -0300

    #9679 Adding test code.

--- src/main/java/io/slingr/endpoints/usps/USPS.java
+++ src/main/java/io/slingr/endpoints/usps/USPS.java
@@ -4,6 +4,8 @@ import io.slingr.endpoints.HttpEndpoint;
 import io.slingr.endpoints.framework.annotations.EndpointFunction;
 import io.slingr.endpoints.framework.annotations.EndpointProperty;
 import io.slingr.endpoints.framework.annotations.SlingrEndpoint;
+import io.slingr.endpoints.utils.Json;
+import io.slingr.endpoints.ws.exchange.FunctionRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,17 +38,30 @@ public class USPS extends HttpEndpoint {
 
 
     @EndpointFunction(name = "_track")
-    public void track() {
+    public Json track(FunctionRequest request) {
 
         logger.info(String.format("User: [%s] and password: [%s]", username, password));
+
+        logger.info(request.toString());
 
         String url = "/ShippingAPITest.dll?API=Verify";
         url += "&XML=<AddressValidateRequest USERID=\"" + username + "\"><Address ID=\"0\"><Address1></Address1>";
         url += "<Address2>6406 Ivy Lane</Address2><City>Greenbelt</City><State>MD</State>";
         url += "<Zip5></Zip5><Zip4></Zip4></Address></AddressValidateRequest>";
 
+        logger.info(url);
 
+        request.getJsonParams().set("path", url);
 
+        Json res = defaultGetRequest(request);
+
+        /**
+         * In ui-builder console
+         * var res = app.endpoints.usps._track();
+         * log(JSON.string(res));
+         */
+
+        return res;
 
     }
 
